bf2c3b5e6b76dc82b0a5e8f4e6bd52fa7b97e459
Bartosz Biegajło <biegbart.z@gmail.com>
2021-05-11 14:54:25 +0200

    change hardcoded form countries to hashmap in a Student class

--- src/main/java/com/luv2code/springdemo/mvc/Student.java
+++ src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
@@ -7,8 +9,17 @@ public class Student {
 	
 	private String country;
 	
+	private LinkedHashMap<String, String> countryOptions;
+	
 	public Student() {
 		
+		countryOptions = new LinkedHashMap<String, String>();
+		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States");
 	}
 
 	public String getFirstName() {
@@ -33,5 +44,9 @@ public class Student {
 
 	public void setCountry(String country) {
 		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 }
